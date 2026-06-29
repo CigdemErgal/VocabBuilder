9289dd7e36e877bed7fabe193d0f70a816f7715d
Cigdem Ergal <cigdemtahtasiz@gmail.com>
2026-06-29 15:58:04 +0300

    bottom tab navigator is done

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -3,8 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DictionaryScreen from "../screens/app/DictionaryScreen";
 import RecommendScreen from "../screens/app/RecommendScreen";
 import TrainingScreen from "../screens/app/TrainingScreen";
-
+import { Image } from "react-native";
 import { VerbType, WordCategory } from "../types/word";
+import { colors } from "../constants/theme";
 
 export type BottomTabParamList = {
   Dictionary:
@@ -31,10 +32,69 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 export function BottomTabNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Dictionary" component={DictionaryScreen} />
-      <Tab.Screen name="Recommend" component={RecommendScreen} />
-      <Tab.Screen name="Training" component={TrainingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+
+        tabBarStyle: {
+          height: 84,
+          paddingTop: 0,
+          paddingBottom: 17,
+          borderTopWidth: 0.5,
+
+          backgroundColor: colors.brand,
+        },
+        tabBarActiveTintColor: colors.background,
+        tabBarInactiveTintColor: "rgba(252, 252, 252, 0.5)",
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: "400",
+          marginTop: 2,
+        },
+        tabBarItemStyle: {
+          paddingVertical: 2,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Dictionary"
+        component={DictionaryScreen}
+        options={{
+          tabBarLabel: "Dictionary",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/dictionary-icon.png")}
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Recommend"
+        component={RecommendScreen}
+        options={{
+          tabBarLabel: "Recommend",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/recommend-icon.png")}
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Training"
+        component={TrainingScreen}
+        options={{
+          tabBarLabel: "Training",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/training-icon.png")}
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
